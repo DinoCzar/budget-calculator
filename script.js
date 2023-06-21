fd66ab2123cab12dc0e528640166099447f6e000
DinoCzar <ryanb_booking@yahoo.com>
2023-06-20 23:23:21 -0700

    Add DOM selectors and display

---
+++ script.js
@@ -0,0 +1,25 @@
+const sales = document.getElementById('sales');
+const card = document.getElementById('card');
+const driverHrs = document.getElementById('driver-hrs');
+const managerHrs = document.getElementById('manager-hrs');
+const budget = document.getElementById('budget');
+const monthPay = document.getElementById('month-pay');
+const weekPay = document.getElementById('week-pay');
+const hourPay = document.getElementById('hour-pay');
+const onCall = document.getElementById('on-call');
+const delivery = document.getElementById('delivery');
+const inventory = document.getElementById('inventory');
+const hours = document.getElementById('hours');
+
+sales.textContent = 'test';
+card.textContent = 'test';
+driverHrs.textContent = 'test';
+managerHrs.textContent = 'test';
+budget.textContent = 'test';
+monthPay.textContent = 'test';
+weekPay.textContent = 'test';
+hourPay.textContent = 'test';
+onCall.textContent = 'test';
+delivery.textContent = 'test';
+inventory.textContent = 'test';
+hours.textContent = 'test';
